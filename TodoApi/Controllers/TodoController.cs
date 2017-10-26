5cf9cb10824cbf6763bcd90a4d6b0e6aa16b129d
Ploy <thongtra.p.naksuksri@gmail.com>
2017-10-26 11:06:26 +0700

    Added swagger and updated Produces and ProducesResponseType

--- TodoApi/Controllers/TodoController.cs
+++ TodoApi/Controllers/TodoController.cs
@@ -4,9 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using TodoApi.Repository;
 using TodoApi.Models;
+using System.Net;
 
 namespace TodoApi.Controllers
 {
+    /// <summary>
+    /// Todo Api
+    /// </summary>
+    [Produces("application/json")]
     [Route("api/[controller]")]
     public class TodoController : Controller
     {
@@ -18,13 +23,26 @@ namespace TodoApi.Controllers
             _repo = repo;
         }
 
+        /// <summary>
+        /// Get list of Todo items
+        /// </summary>
+        /// <returns>List of Todo items</returns>
         [HttpGet]
+        [ProducesResponseType(typeof(List<TodoItem>),200)]
         public async Task<IEnumerable<TodoItem>> GetAll() 
         {
             return await _repo.GetAll();
         }
 
+        /// <summary>
+        /// Get Todo item with specific Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <response code="404">If the item is not found</response>
         [HttpGet("{id}", Name="GetToDo")]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [Produces("application/json", Type = typeof(TodoItem))]
         public async Task<IActionResult> GetById(long id)
         {
             TodoItem item = await _repo.GetById(id);
@@ -36,7 +54,16 @@ namespace TodoApi.Controllers
             return new ObjectResult(item);
         } 
 
+        /// <summary>
+        /// Create a new Todo item
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns>a newly created Todo item</returns>
+        /// <response code="201">Returns the newly-created item</response>
+        /// <response code="400">If the item is null</response>          
         [HttpPost]
+        [ProducesResponseType(typeof(TodoItem), 201)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
         public async Task<IActionResult> CreateItem([FromBody]TodoItem item)
         {
             if (item == null)
@@ -48,7 +75,19 @@ namespace TodoApi.Controllers
 
         }
 
+        /// <summary>
+        /// Update Todo item
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        /// <response code="400">If the item is null or id isn't matched</response>
+        /// <response code="404">If the item is not found</response>
+        /// <response code="204">Item is updated successfully</response>
         [HttpPut("{id}")]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
         public async Task<IActionResult> Update(long id, [FromBody]TodoItem item)
         {
             if (item == null || item.Id != id)
@@ -68,7 +107,16 @@ namespace TodoApi.Controllers
             return new NoContentResult();            
         }
 
+        /// <summary>
+        /// Delete item with specific id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <response code="404">If the item is not found</response>
+        /// <response code="204">If the item is deleted<response>
         [HttpDelete("{id}")]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
         public async Task<IActionResult> Delete(long id)
         {
             var todo = await _repo.GetById(id);
--- TodoApi/Startup.cs
+++ TodoApi/Startup.cs
@@ -9,6 +9,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.PlatformAbstractions;
+using System.IO;
+using Swashbuckle.AspNetCore.Swagger;
 using TodoApi.Models;
 using TodoApi.Repository;
 
@@ -29,6 +32,19 @@ namespace TodoApi
             // Add DbContext - use In memory database
             services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
             services.AddScoped<ITodoItemsRepository, TodoItemsRepository>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info 
+                { 
+                    Version ="1.0",
+                    Title = "Todo Api", 
+                    Description = "Document for Todo Api"                   
+                });
+
+                var basePath = PlatformServices.Default.Application.ApplicationBasePath;
+                var xmlPath = Path.Combine(basePath, "TodoApi.xml"); 
+                c.IncludeXmlComments(xmlPath);   
+            });
             services.AddMvc();
         }
 
@@ -39,6 +55,12 @@ namespace TodoApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => 
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo Api v1");
+            });
 
             app.UseMvc();
         }
